d2082385f3023368f29b4a61f40478fa21250532
ibrahimwichka <wikipediaofsoccer@gmail.com>
2024-02-11 12:45:20 -0500

    new commit

--- utils/drugLikeness.py
+++ utils/drugLikeness.py
@@ -151,7 +151,7 @@ def check_num_violations(smiles):
     if num_of_violations == 1:
         return "Only 1 violation: Molecule is likely drug-like", "green" 
     if num_of_violations == 2 or num_of_violations == 3:
-        return "Only 2 violations: Molecules may or may not be drug-like", "green"
+        return "Only " + str(str(num_of_violations)) +  " violations: Molecules may or may not be drug-like", "green"
     if num_of_violations == 0:
         return "0 violations: Molecule is  Drug-Like", "green"
     if num_of_violations > 3:
